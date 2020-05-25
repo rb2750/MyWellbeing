89b445d2661fd92d7e39b3fa5ce7eada993e1b72
Rob <rob@bladen.me>
2020-05-25 20:23:24 +0100

    Disable enter name screen.

--- src/main.dart
+++ src/main.dart
@@ -38,7 +38,8 @@ class MyWellbeingState extends State<MyWellbeing> {
   }
 
   void goToPage(int index) {
-    _controller.animateToPage(index, duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
+    _controller.animateToPage(index,
+        duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
   }
 
   @override
@@ -48,8 +49,10 @@ class MyWellbeingState extends State<MyWellbeing> {
     MaterialColor color1 = Colors.red;
     MaterialColor color2 = Colors.yellow;
     MaterialColor color3 = Colors.green;
-    Color backgroundColor =
-        Color.lerp(Color.lerp(color1[200], color2[200], (MyWellbeing.emotion / 100)), Color.lerp(color2[200], color3[200], (MyWellbeing.emotion / 100)), (MyWellbeing.emotion / 100));
+    Color backgroundColor = Color.lerp(
+        Color.lerp(color1[200], color2[200], (MyWellbeing.emotion / 100)),
+        Color.lerp(color2[200], color3[200], (MyWellbeing.emotion / 100)),
+        (MyWellbeing.emotion / 100));
 
     return MaterialApp(
         title: 'MyWellbeing',
@@ -58,85 +61,126 @@ class MyWellbeingState extends State<MyWellbeing> {
         ),
         home: FutureBuilder(
             future: SharedPreferences.getInstance(),
-            builder: (BuildContext context, AsyncSnapshot<SharedPreferences> prefs) {
+            builder:
+                (BuildContext context, AsyncSnapshot<SharedPreferences> prefs) {
+              MyWellbeing.name = "John"; //TODO TEMP
+
+              if (prefs.hasData && prefs.data.containsKey("Name"))
+                MyWellbeing.name = prefs.data.getString("Name");
+
               Widget widget = Container();
 
-              if (prefs.hasData) {
-                if (prefs.data.containsKey("Name")) {
-                  MyWellbeing.name = prefs.data.getString("Name");
-                  widget = Scaffold(
-                      body: Container(
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              begin: Alignment.topRight,
-                              end: Alignment.bottomLeft,
-                              stops: [0, 1],
-                              colors: [Colors.blue[500], Colors.blue[900]],
-                            ),
+              if (MyWellbeing.name != null) {
+                widget = Scaffold(
+                    body: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            stops: [0, 1],
+                            colors: [Colors.blue[500], Colors.blue[900]],
                           ),
-                          child: Stack(children: <Widget>[
-                            PageView(
-                              physics: new NeverScrollableScrollPhysics(),
-                              controller: _controller,
-                              children: <Widget>[Home(firstLaunch: fl), Charts()],
-                            ),
-                            FadeUp(
-                                18,
-                                Column(
-                                  children: <Widget>[
-                                    Spacer(),
-                                    Visibility(
-                                        visible: MyWellbeing.selectedEmotion != null,
-                                        child: Container(
-                                            margin: EdgeInsets.only(bottom: 5, left: 5, right: 5),
-                                            padding: EdgeInsets.all(10),
-                                            decoration: BoxDecoration(color: Colors.blueGrey.shade400.withOpacity(0.75), borderRadius: BorderRadius.all(Radius.circular(10))),
-                                            child: Row(
-                                              mainAxisAlignment: MainAxisAlignment.start,
-                                              children: <Widget>[
-                                                Expanded(
-                                                    child: Container(
-                                                        margin: EdgeInsets.only(right: 10),
-                                                        child: AutoSizeText(
-                                                          "Selected emotion: " + (MyWellbeing.selectedEmotion != null ? MyWellbeing.selectedEmotion.name() : "None"),
-                                                          style: TextStyle(fontSize: 30, color: Colors.white70),
-                                                          maxLines: 1,
-                                                        ))),
-                                                MaterialButton(
-                                                    child: Text("Change", style: TextStyle(color: Colors.white)),
-                                                    color: Color.fromRGBO(backgroundColor.red - 50, backgroundColor.green - 50, backgroundColor.blue - 50, 1),
-                                                    onPressed: () {})
-                                              ],
-                                            ))),
-                                    Container(
-                                        margin: EdgeInsets.only(bottom: 10, left: 10, right: 10),
-                                        padding: EdgeInsets.only(bottom: 5),
-                                        decoration: BoxDecoration(color: Colors.blueGrey.shade400.withOpacity(0.85), borderRadius: BorderRadius.all(Radius.circular(10))),
-                                        child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                          children: <Widget>[
-                                            IconButton(
-                                              icon: Icon(Icons.add_circle, color: currentPage == 0 ? Colors.white : Colors.blueGrey[200], size: 40),
-                                              onPressed: () {
-                                                goToPage(0);
-                                              },
-                                            ),
-                                            IconButton(
-                                              icon: Icon(Icons.pie_chart, color: currentPage == 1 ? Colors.white : Colors.blueGrey[200], size: 40),
-                                              onPressed: () {
-                                                goToPage(1);
-                                              },
-                                            ),
-                                          ],
-                                        ))
-                                  ],
-                                ),
-                                animate: fl,
-                                onFinish: () => MyWellbeing.firstLaunch = false)
-                          ])));
-                } else {
-                  widget = Splash();
-                }
+                        ),
+                        child: Stack(children: <Widget>[
+                          PageView(
+                            physics: new NeverScrollableScrollPhysics(),
+                            controller: _controller,
+                            children: <Widget>[Home(firstLaunch: fl), Charts()],
+                          ),
+                          FadeUp(
+                              18,
+                              Column(
+                                children: <Widget>[
+                                  Spacer(),
+                                  Visibility(
+                                      visible:
+                                          MyWellbeing.selectedEmotion != null,
+                                      child: Container(
+                                          margin: EdgeInsets.only(
+                                              bottom: 5, left: 5, right: 5),
+                                          padding: EdgeInsets.all(10),
+                                          decoration: BoxDecoration(
+                                              color: Colors.blueGrey.shade400
+                                                  .withOpacity(0.75),
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(10))),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: <Widget>[
+                                              Expanded(
+                                                  child: Container(
+                                                      margin: EdgeInsets.only(
+                                                          right: 10),
+                                                      child: AutoSizeText(
+                                                        "Selected emotion: " +
+                                                            (MyWellbeing.selectedEmotion !=
+                                                                    null
+                                                                ? MyWellbeing
+                                                                    .selectedEmotion
+                                                                    .name()
+                                                                : "None"),
+                                                        style: TextStyle(
+                                                            fontSize: 30,
+                                                            color:
+                                                                Colors.white70),
+                                                        maxLines: 1,
+                                                      ))),
+                                              MaterialButton(
+                                                  child: Text("Change",
+                                                      style: TextStyle(
+                                                          color: Colors.white)),
+                                                  color: Color.fromRGBO(
+                                                      backgroundColor.red - 50,
+                                                      backgroundColor.green -
+                                                          50,
+                                                      backgroundColor.blue - 50,
+                                                      1),
+                                                  onPressed: () {})
+                                            ],
+                                          ))),
+                                  Container(
+                                      margin: EdgeInsets.only(
+                                          bottom: 10, left: 10, right: 10),
+                                      padding: EdgeInsets.only(bottom: 5),
+                                      decoration: BoxDecoration(
+                                          color: Colors.blueGrey.shade400
+                                              .withOpacity(0.85),
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(10))),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: <Widget>[
+                                          IconButton(
+                                            icon: Icon(Icons.add_circle,
+                                                color: currentPage == 0
+                                                    ? Colors.white
+                                                    : Colors.blueGrey[200],
+                                                size: 40),
+                                            onPressed: () {
+                                              goToPage(0);
+                                            },
+                                          ),
+                                          IconButton(
+                                            icon: Icon(Icons.pie_chart,
+                                                color: currentPage == 1
+                                                    ? Colors.white
+                                                    : Colors.blueGrey[200],
+                                                size: 40),
+                                            onPressed: () {
+                                              goToPage(1);
+                                            },
+                                          ),
+                                        ],
+                                      ))
+                                ],
+                              ),
+                              animate: fl,
+                              onFinish: () => MyWellbeing.firstLaunch = false)
+                        ])));
+              } else {
+                widget = Splash();
               }
 
               return widget;
